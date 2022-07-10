30666a54d96715c1403bbbe7b729fdf24b590112
Anton Abramov <ceasar0713@gmail.com>
2022-07-10 22:09:02 +0300

    Сделали 1 задачу

---
+++ HM1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет произведение натуральных чисел в промежутке от M до N.
+// M = 1; N = 5. ->120
+// M = 4; N = 6. -> 120
+
+using System;
+using static System.Console;
+Clear();
+{
+WriteLine("Введите M: ");
+int M = int.Parse(ReadLine());
+WriteLine("Введите  N: ");
+int N = int.Parse(ReadLine());
+
+WriteLine(Product(M, N));
+
+int Product(int a, int b)
+{
+    return a==b? b
+    : a<b? a * Product(++a,b)
+    : b* Product(a, ++b);
+}
+
+}
